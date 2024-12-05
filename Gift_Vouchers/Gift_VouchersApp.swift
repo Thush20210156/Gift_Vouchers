5faef72dd74d02ce2bf86cf9ea07c1d0b7614f3a
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-12-05 08:18:04 +0530

    Initial Commit

---
+++ Gift_Vouchers/Gift_VouchersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gift_VouchersApp.swift
+//  Gift_Vouchers
+//
+//  Created by Thushini Abeysuriya on 2024-12-05.
+//
+
+import SwiftUI
+
+@main
+struct Gift_VouchersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gift_VouchersTests/Gift_VouchersTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gift_VouchersTests.swift
+//  Gift_VouchersTests
+//
+//  Created by Thushini Abeysuriya on 2024-12-05.
+//
+
+import Testing
+@testable import Gift_Vouchers
+
+struct Gift_VouchersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
